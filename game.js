51147e4f874c5cb65a593425a0e7a298619e0102
JayadevArun <jayadevarun03@gmail.com>
2023-05-22 11:58:06 +0530

    Working update

--- game.js
+++ game.js
@@ -1,20 +1,47 @@
 var buttonColour = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userPattern = [];
+
+var level = 0;
+var flag = false;
+
 function nextSeq() {
+
+    level++;
+    $("h1").text("Level " + level);
+
     var randomNumber = Math.floor((Math.random()) * 4);
     var randomColour = buttonColour[randomNumber];
     gamePattern.push(randomColour);
+
     $("#" + randomColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomColour);
+
 $(".btn").on("click", function () {
     var userColour = $(this).attr("id");
     userPattern.push(userColour);
     playSound(userColour);
+
+
+
 function playSound(colour) {
+
 function Animation(colour) {
     $("#" + colour).addClass("pressed");
     setTimeout(function () {
         $("#" + colour).removeClass("pressed");
     }, 100);
 }
+
+$(document).on("keydown", function () {
+    if (flag == false) {
+        $("h1").text("Level " + level);
+        nextSeq();
+        flag = true;
+    }
+})
+
+
+
+
+
