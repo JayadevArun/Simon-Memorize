2ff6f2778e67683a911042275031a2e4c895be46
JayadevArun <jayadevarun03@gmail.com>
2023-05-22 09:48:34 +0530

    Working update

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 var buttonColour = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userPattern = [];
 function nextSeq() {
     var randomNumber = Math.floor((Math.random()) * 4);
     var randomColour = buttonColour[randomNumber];
     gamePattern.push(randomColour);
     $("#" + randomColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomColour);
+$(".btn").on("click", function () {
+    var userColour = $(this).attr("id");
+    userPattern.push(userColour);
 function playSound(colour) {
